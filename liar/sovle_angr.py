003da2739a771f79e1c122271d3e0cbdb3ab0871
Your Name <you@example.com>
2021-06-26 02:00:51 -0700

    a

--- liar/sovle_angr.py
+++ liar/sovle_angr.py
@@ -7,9 +7,9 @@ def main():
     sm = project.factory.simulation_manager(init_state)
     sm.explore(find=0x00000000000009B0)
     found = sm.found[0]
-    solution = found.solver.eval(cast_to=bytes)
-    return solution
+ #   solution = found.solver.eval(cast_to=bytes)
+    return found
 
 
 if __name__ == '__main__':
-    print(repr(main()))
+    print(main())
