11980ebc6deb1f39dce209aba4d1c9b0c1a35378
Wayne <52121443+kddco@users.noreply.github.com>
2021-06-26 16:56:47 +0800

    1

--- liar/sovle_angr.py
+++ liar/sovle_angr.py
@@ -6,8 +6,9 @@ def main():
     init_state = project.factory.entry_state()
     sm = project.factory.simulation_manager(init_state)
     sm.explore(find=0x00000000000009B0)
-    sm.run()
-    return sm.found[0]
+    found = sm.found[0]
+    solution = found.solver.eval(cast_to=bytes)
+    return solution
 
 
 if __name__ == '__main__':
