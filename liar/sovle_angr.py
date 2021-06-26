78439897b52fe8c3c2e3119575c2d3b06aeb103c
Wayne <52121443+kddco@users.noreply.github.com>
2021-06-26 16:53:33 +0800

    1

--- liar/sovle_angr.py
+++ liar/sovle_angr.py
@@ -3,7 +3,7 @@ import claripy
 
 def main():
     project = angr.Project("/home/kddc/Downloads/liar")
-    init_state = project.factory.entry_state("/home/kddc/Downloads/liar")
+    init_state = project.factory.entry_state()
     sm = project.factory.simulation_manager(init_state)
     sm.explore(find=0x00000000000009B0)
     sm.run()
